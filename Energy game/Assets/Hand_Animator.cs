4f53d39c58ef15afc01428af703edd5b5c8aa3fc
Aiuro1703 <149496475+Aiuro1703@users.noreply.github.com>
2025-04-23 13:57:17 +0100

    finishing touches

--- Energy game/Assets/Hand_Animator.cs
+++ Energy game/Assets/Hand_Animator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
@@ -9,7 +10,12 @@ public class Hand_Animator : MonoBehaviour
 {
     [SerializeField] private NearFarInteractor nearFarInteractor;
     [SerializeField] private SkinnedMeshRenderer HandMesh;
+    [SerializeField] private InputActionReference selectActionRef;
+    [SerializeField] private InputActionReference activateActionRef;
+    [SerializeField] private Animator Handanimator;
 
+    private static readonly int activateAnim = Animator.StringToHash("activate");
+    private static readonly int selectAnim = Animator.StringToHash("select");
     private void Awake()
     {
         nearFarInteractor.selectEntered.AddListener(OnGrab);
@@ -30,6 +36,7 @@ public class Hand_Animator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Handanimator.SetFloat("activate", activateActionRef.action.ReadValue<float>());
+        Handanimator.SetFloat("select", selectActionRef.action.ReadValue<float>());
     }
 }
